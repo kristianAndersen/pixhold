293b033ac1ba7cf4abf62ac35c5121d92fa40a7e
kristian andersen <krilleandersen@gmail.com>
2020-12-09 15:39:00 +0100

    Add files via upload
    initial commit

---
+++ api/pixhold.js
@@ -0,0 +1,32 @@
+const  fabric = require('fabric').fabric;
+
+let fcanvas = new fabric.StaticCanvas(null, {width:200,height:200});
+
+
+
+
+module.exports = async (req, res) => {
+
+    
+
+    var placeholder = new fabric.Rect({
+        name:placeholder,
+        hasControls: false,
+        selectable : false,
+        left: 0,
+        top: 0,
+        fill: '#a0d323',
+        width: 590,
+        height: 222
+    });
+    
+    fcanvas.add(placeholder);
+
+
+    let svgoutput = fcanvas.toSVG();
+
+   
+    
+   res.send(`Hello ${req.body}, you just parsed the request body!`)
+
+};
---
+++ index.js
@@ -0,0 +1,1 @@
+let pix = require('/api/pixhold');
