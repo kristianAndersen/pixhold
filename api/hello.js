5b29bb5ae76bb94d13a45bcf4b325930bb64ffcb
kristian andersen <krilleandersen@gmail.com>
2020-12-09 17:09:33 +0100

    Add files via upload

--- api/hello.js
+++ api/hello.js
@@ -1,13 +1,7 @@
-import Status from 'http-status-codes';
 
-// GET -> http://localhost:3000/api/hello?name=Geoffrey
+//api/hello?name=Geoffrey
 
-export default (request, response) => {
-  if (request.method !== 'GET') {
-    return response.status(Status.BAD_REQUEST).send('');
-  }
-  const name = request?.query?.name ?? 'world';
-  return response.json({
-    data: `hello ${name}`,
-  });
+export default (req, res) => {
+  const { name = 'World' } = req.query
+  res.status(200).send(`Hello ${name}!`)
 };
