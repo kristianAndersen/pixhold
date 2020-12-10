ed7e88f57e9e45f8ccd3458dedc95295d308781e
kristian andersen <krilleandersen@gmail.com>
2020-12-10 16:06:49 +0100

    Add files via upload
    fixed  res.setHeader("Content-Type", "image/svg+xml");

--- api/hello.js
+++ api/hello.js
@@ -3,6 +3,8 @@
 
 export default (req, res) => {
 
+  res.setHeader("Content-Type", "image/svg+xml");
+  
   const { ph = 'pix' } = req.query
   let ary = ph.split(",");
 
@@ -11,29 +13,13 @@ export default (req, res) => {
   let color=ary[2];
   let text=width+'X'+height;
  
-  let tx=width/2;
-  let ty=height/2;
 
-
-let svg=`<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
-  <rect fill="${color}" width="${width}" height="${height}"/>
-  <text fill="rgba(0,0,0,0.5)" font-family="sans-serif" font-size="10" dy="10.5" font-weight="bold" x="50%" y="50%" text-anchor="middle">${text}</text>
+let svg=`<svg xmlns='http://www.w3.org/2000/svg' width='${width}' height='${height}' viewBox='0 0 ${width} ${height}'>
+<rect fill='#${color}' width='${width}' height='${height}'/>
+<text fill='rgba(0,0,0,0.5)' font-family='sans-serif' font-size='10' dy='10.5' font-weight='bold' x='50%' y='50%' text-anchor='middle'>${text}
+</text>
 </svg>`
 
-
-const cleaned = svg
-  .replace(/[\t\n\r]/gim, '') // Strip newlines and tabs
-  .replace(/\s\s+/g, ' ') // Condense multiple spaces
-  .replace(/'/gim, '\\i'); // Normalize quotes
-
-const encoded = encodeURIComponent(cleaned)
-  .replace(/\(/g, '%28') // Encode brackets
-  .replace(/\)/g, '%29');
-
-let cleansvg=`data:image/svg+xml;charset=UTF-8,${encoded}`;
-
-res.setHeader('Content-Type', 'image/svg+xml');  
-res.status(200).send(cleansvg)
-
+res.status(200).send(svg)
 
 };
